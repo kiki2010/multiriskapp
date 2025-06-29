464305068170c755aa94d35022fe54cf9eaf25c7
kiki2010 <chiaraangelinacatalini@gmail.com>
2025-06-29 15:21:13 -0300

    started working on the AI implementation

---
+++ app/multiriskapp/lib/predict.dart
@@ -0,0 +1,7 @@
+import 'package:multiriskapp/weatherstationsdata.dart';
+import 'dart:convert';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'dart:typed_data';
+
+//Flood AI model
+
