3ecf101a92689164dd06ef53a581564f564ef688
kiki2010 <chiaraangelinacatalini@gmail.com>
2025-07-05 12:40:23 -0300

    working on the near cordoba screen, at least the style aspect

--- app/multiriskapp/lib/Screens/nearcordoba.dart
+++ app/multiriskapp/lib/Screens/nearcordoba.dart
@@ -4,12 +4,47 @@ class nearCordobaScreen extends StatefulWidget {
   const nearCordobaScreen({super.key});
 
   @override
-  State<nearCordobaScreen> createState() => _NearcordobaState();
+  State<nearCordobaScreen> createState() => _nearCordobaScreenState();
 }
 
-class _NearcordobaState extends State<nearCordobaScreen> {
+class _nearCordobaScreenState extends State<nearCordobaScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Places Near Cordoba"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+         const Divider(),
+         DropdownButton(
+          value: "",
+          onChanged: (value) {
+            
+          },
+          items: const [
+            DropdownMenuItem<String>(
+              value: 'all',
+              child: Text("All"),
+            ),
+            DropdownMenuItem<String>(
+              value: 'ForestandParks',
+              child: Text("Forest and Parks"),
+            ),
+            DropdownMenuItem<String>(
+              value: 'Beach',
+              child: Text("Beachs"),
+            ),
+            DropdownMenuItem<String>(
+              value: 'Campings',
+              child: Text("Campings"),
+            ),
+          ],
+         ),
+         const Divider(), 
+        ],
+      ),
+    );
   }
 }
