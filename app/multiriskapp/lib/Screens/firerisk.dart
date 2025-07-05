e5c9e95be005438ac98cc814e323e450e4cd9aa8
kiki2010 <chiaraangelinacatalini@gmail.com>
2025-07-05 11:48:00 -0300

    commented fire risk screen

--- app/multiriskapp/lib/Screens/firerisk.dart
+++ app/multiriskapp/lib/Screens/firerisk.dart
@@ -14,13 +14,15 @@ class FireScreen extends StatefulWidget {
 
 class _FireScreenState extends State<FireScreen> {
   final WeatherStationService _weatherService = WeatherStationService();
-
+  
+  //Maps for saving the data get on the weather API
   Map<String, dynamic>? actualData;
   String? fireRiskLevel;
 
   bool isLoading = true;
   String? error;
 
+  //Getting the meteorological data and AI prediction, executing the funtions in charge of that
   @override
   void initState() {
     super.initState();
@@ -28,11 +30,13 @@ class _FireScreenState extends State<FireScreen> {
     _predictFireRisk();
   }
 
+  //Get weather data
   Future<void> _loadWeatherData() async {
     try {
       await _weatherService.getNearestStation(widget.position);
       final actual = await _weatherService.getActualData(widget.position);
 
+      //Updating the values ​​of the variables with the data obtained
       setState(() {
         actualData = actual;
         isLoading = false;
@@ -44,14 +48,16 @@ class _FireScreenState extends State<FireScreen> {
       });
     }
   }
-
+  
+  //AI prediction
   Future<void> _predictFireRisk() async {
     Position pos = await Geolocator.getCurrentPosition();
 
     final predictor = FirePrediction();
     await predictor.loadFireModel();
     await predictor.predictFire(pos);
-
+    
+    //Save the flood risk
     setState(() {
       fireRiskLevel = predictor.fireRiskLevel;
     });
@@ -64,6 +70,7 @@ class _FireScreenState extends State<FireScreen> {
     Color fireRiskColor;
     String fireRiskText;
 
+    //Depending on the case, determine a text and color
     switch(fireRiskLevel) {
       case 'LOW':
         fireRiskText = 'Low';
@@ -82,8 +89,11 @@ class _FireScreenState extends State<FireScreen> {
         fireRiskColor = Colors.grey;
     }
 
+    //Scaffold of the app
     return Scaffold(
+      //Title of the screen
       appBar: AppBar(title: const Text("Fire Risk Screen")),
+      //Body with a Circular Progress Indicator, until the data is obtained
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : error != null
@@ -93,6 +103,7 @@ class _FireScreenState extends State<FireScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
+                      //Divider, all the data, but the Risk Icon is outside a sizedBox, I think Dividers looks cool
                       const Divider(),
                       Icon(Icons.local_fire_department, color: fireRiskColor, size: 65,),
                       Text("Fire Risk: $fireRiskText"),
